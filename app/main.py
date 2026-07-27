8bb12ec711b3bf8ff1ffbc68c6a2bb95e2e41b54
aryan <aryanpc79@gmail.com>
2026-07-28 03:28:08 +0530

    added fornend links

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://frontend-one-theta-tznvtxa4h7.vercel.app",
         "http://127.0.0.1:5500",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
